a09057896acf54b152803ce26d84a9fbc7e4a6db
Wim Van Leuven <wim.vanleuven@bigboards.io>
2019-02-03 13:26:21 +0100

    fix footer floating on mobile and weed out legacy code

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,3 @@ $(window).load(function() {
   // Animate loader off screen
   $(".se-pre-con").fadeOut("slow");
 });
-
-$(document).ready(function(){
-  $("#counter").countdown({
-  until: new Date(2019, 9 - 1, 23),
-  format: 'dHMS'
-  });
-
-  $("#counter_wrapper").fitText(1.2, {
-  minFontSize: '20px',
-  maxFontSize: '50px'
-  });
-});
