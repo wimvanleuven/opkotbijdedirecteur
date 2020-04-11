913ffdade343c29e312b9d23d6d9c7ae16ec5142
Wim Van Leuven <wim.vanleuven@bigboards.io>
2020-04-11 12:45:05 +0200

    improve typed tekst

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,14 +5,14 @@
                 <div class="md-layout">
                     <div class="md-layout-item md-size-70 md-small-size-70 md-xsmall-size-100">
                         <h1 class="title">opkotbijdedirecteur</h1>
-                        <h2>
-                            <vue-typed-js
+                        <vue-typed-js
                                 :strings="catchphrases"
                                 :loop="true"
-                            >
-                                <span class="typing" />
-                            </vue-typed-js>
-                        </h2>
+                        >
+                            <h2>
+                                <span class="typing"/>
+                            </h2>
+                        </vue-typed-js>
                     </div>
                 </div>
             </div>
@@ -44,9 +44,9 @@
         computed: {
             catchphrases() {
                 return [
-                    "In een historisch kader.",
-                    "In alle luxe en comfort.",
-                    "In <red>♥</red>je Leuven."
+                    "Da's op kot gaan in een historisch kader",
+                    "Da's op kot gaan in alle luxe en comfort",
+                    "Da's op kot gaan in <red>♥</red>je Leuven"
                 ]
             },
             headerStyle() {
@@ -59,7 +59,7 @@
 </script>
 
 <style>
-red {
-    color: red
-}
+    red {
+        color: red
+    }
 </style>
