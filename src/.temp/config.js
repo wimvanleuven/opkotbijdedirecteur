edc4dbef171cf267979ecd27275dfd5d8be718d0
Wim Van Leuven <wim.vanleuven@bigboards.io>
2020-04-26 11:37:09 +0200

    yarn upgrade

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s | okbdd",
-  "siteUrl": "https://opkotbijdedirecteur.be",
-  "version": "0.7.13",
-  "catchLinks": true
-}
