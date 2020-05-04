cc3054ff59775ad8cecb8f674340dbfb1126e5fc
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2020-05-04 22:45:00 +0200

    no prefix for deploy via gh-pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -20,7 +20,6 @@ module.exports = {
   siteName: 'opkotbijdedirecteur',
   siteDescription: 'opkotbijdedirecteur is op kot gaan in een historisch kader met alle luxe en comfort in hartje Leuven',
   siteUrl: 'https://opkotbijdedirecteur.be',
-  pathPrefix: '/opkotbijdedirecteur',
   titleTemplate: '%s | opkotbijdedirecteur',
   plugins: [{
       use: 'gridsome-plugin-robots-txt',
